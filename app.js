94b2b6e341e374704e3ada107f92557d9341c50f
ANDREW JAMES BOYLE <ANDREWBOYLEMUSIC@GMAIL.COM>
2022-08-11 12:28:06 -0700

    user can vote in current poll!

--- app.js
+++ app.js
@@ -3,39 +3,49 @@ import { renderPoll } from './render-utils.js';
 
 const pollFormEl = document.getElementById('form-input');
 const currentPollEl = document.getElementById('current-poll-container');
+const voteABtn = document.getElementById('vote-a');
+const voteBBtn = document.getElementById('vote-b');
 
 // let state
 
 const currentPoll = {
-  question: '',
-  optionA: '',
-  optionB: '',
-  votesA: 0,
-  votesB: 0
+    question: '',
+    optionA: '',
+    optionB: '',
+    votesA: 0,
+    votesB: 0
 };
 
 
 
 pollFormEl.addEventListener('submit', (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const data = new FormData(pollFormEl);
+    const data = new FormData(pollFormEl);
   
-  currentPoll.question = data.get('question');
-  currentPoll.optionA = data.get('option-a');
-  currentPoll.optionB = data.get('option-b');
+    currentPoll.question = data.get('question');
+    currentPoll.optionA = data.get('option-a');
+    currentPoll.optionB = data.get('option-b');
 
-  displayCurrentPoll();
+    displayCurrentPoll();
 
-  pollFormEl.reset();
-})
+    pollFormEl.reset();
+});
 
+voteABtn.addEventListener('click', () => {
+    currentPoll.votesA++;
+    displayCurrentPoll();
+});
 
+voteBBtn.addEventListener('click', () => {
+    currentPoll.votesB++;
+    displayCurrentPoll();
+});
 
 
 
 function displayCurrentPoll() {
-  currentPollEl.textContent = '';
-  const currentPollData = renderPoll(currentPoll);
-  currentPollEl.append(currentPollData);
+    currentPollEl.textContent = '';
+    const currentPollData = renderPoll(currentPoll);
+    currentPollEl.append(currentPollData);
 }
