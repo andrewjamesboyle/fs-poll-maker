9e61a032c42c703ff8bd6c0ec842695eb6166517
ANDREW JAMES BOYLE <ANDREWBOYLEMUSIC@GMAIL.COM>
2022-08-11 11:56:26 -0700

    pollFormEl captures user input and console.logs

--- app.js
+++ app.js
@@ -6,11 +6,34 @@ const pollFormEl = document.getElementById('form-input');
 
 // let state
 
+const currentPoll = {
+  question: '',
+  optionA: '',
+  optionB: '',
+  votesA: 0,
+  votesB: 0
+};
 
 
 
 pollFormEl.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  console.log('form submitted');
+  const data = new FormData(pollFormEl);
+  
+  currentPoll.question = data.get('question');
+  currentPoll.optionA = data.get('option-a');
+  currentPoll.optionB = data.get('option-b');
+
+  displayCurrentPoll();
+
+  pollFormEl.reset();
 })
+
+
+
+
+
+function displayCurrentPoll() {
+  
+}
